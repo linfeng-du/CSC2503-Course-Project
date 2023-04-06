305b082967d7c42c9d3b27d2aea3ad08b006e833
CapFreddy <linfeng9918@gmail.com>
2023-04-06 17:43:31 -0400

    update load_data.py (add collate function)

--- src/load_data.py
+++ src/load_data.py
@@ -115,6 +115,65 @@ class HomographyEstimationDataset(torch.utils.data.Dataset):
         return kpts, desc, scores
 
 
+def collate_function(batch, num_keypoints):
+    """Pad/truncate and collate batched items"""
+    M_list = []
+    kpts0_list, kpts1_list = [], []
+    desc0_list, desc1_list = [], []
+    scores0_list, scores1_list = [], []
+    mask0_list, mask1_list = [], []
+    for data in batch:
+        kpts0, desc0, scores0, mask0 = _pad_truncate_tensors(data['keypoints0'],
+                                                             data['descriptors0'],
+                                                             data['scores0'],
+                                                             num_keypoints)
+        kpts1, desc1, scores1, mask1 = _pad_truncate_tensors(data['keypoints1'],
+                                                             data['descriptors1'],
+                                                             data['scores1'],
+                                                             num_keypoints)
+
+        M_list.append(data['M'])
+        kpts0_list.append(kpts0)
+        kpts1_list.append(kpts1)
+        desc0_list.append(desc0)
+        desc1_list.append(desc1)
+        scores0_list.append(scores0)
+        scores1_list.append(scores1)
+        mask0_list.append(mask0)
+        mask1_list.append(mask1)
+
+    batch = {
+        'M': torch.stack(M_list),
+        'keypoints0': torch.stack(kpts0_list),
+        'keypoints1': torch.stack(kpts1_list),
+        'descriptors0': torch.stack(desc0_list),
+        'descriptors1': torch.stack(desc1_list),
+        'scores0': torch.stack(scores0_list),
+        'scores1': torch.stack(scores1_list),
+        'mask0': torch.stack(mask0_list),
+        'mask1': torch.stack(mask1_list)
+    }
+    return batch
+
+
+def _pad_truncate_tensors(kpts, desc, scores, num_keypoints):
+    """Pad/truncate input tensors to the lenght of num_keypoints"""
+    mask = torch.ones(num_keypoints, dtype=torch.bool)
+    if len(kpts) >= num_keypoints:
+        kpts = kpts[:num_keypoints, :]
+        desc = desc[:, :num_keypoints]
+        scores = scores[:num_keypoints]
+        return kpts, desc, scores, mask
+
+    num_pad = num_keypoints - len(kpts)
+    kpts = torch.concat((kpts, torch.zeros(num_pad, 2)))
+    desc = torch.concat((desc, torch.zeros(num_pad, desc.size(1))))
+    scores = torch.concat((scores, torch.zeros(num_pad)))
+    mask[-num_pad:] = 0
+
+    return kpts, desc, scores, mask
+
+
 if __name__ == '__main__':
     from tqdm import tqdm
     from parse_args import parse_args
