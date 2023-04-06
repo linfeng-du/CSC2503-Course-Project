571f1de93e27608da4ffadb68260763fa52b45bc
CapFreddy <linfeng9918@gmail.com>
2023-04-05 21:58:40 -0400

    update parse_args.py

--- src/parse_args.py
+++ src/parse_args.py
@@ -5,6 +5,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # HomographyEstimationDataset
+    parser.add_argument('--dataset', choices=['Oxford and Paris', 'COCO'], default='Oxford and Paris')
     parser.add_argument('--num_keypoints', type=int, default=1024)
     parser.add_argument('--resize', type=int, nargs='+', default=[640, 480])
     parser.add_argument('--descriptor', choices=['SIFT', 'SuperPoint'], default='SIFT')
@@ -12,14 +13,23 @@ def parse_args():
     # SuperPoint
     parser.add_argument('--nms_radius', type=int, default=4)
     parser.add_argument('--keypoint_threshold', type=float, default=0.005)
+    parser.add_argument('--remove_borders', type=int, default=4)
 
     # SuperGlue
+    parser.add_argument('--weights', choices=['none', 'indoor', 'outdoor'], default='none')
     parser.add_argument('--sinkhorn_iterations', type=int, default=100)
     parser.add_argument('--match_threshold', type=float, default=0.2)
 
     # Training
+    parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--num_epochs', type=int, default=20)
     parser.add_argument('--batch_size', type=int, default=24)
     parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--gamma', type=float, default=0.999998)
 
+    # Reproducibility
+    parser.add_argument('--homo_seed', type=int, default=42)
+    parser.add_argument('--data_seed', type=int, default=42)
+    parser.add_argument('--model_seed', type=int, default=42)
+    parser.add_argument('--training_seed', type=int, default=42)
     return parser.parse_args()
