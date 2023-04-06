b6f900c22d368faa23d93669ee6a4caeb0fb7c1d
CapFreddy <linfeng9918@gmail.com>
2023-04-06 17:45:08 -0400

    update debug.py

--- debug.py
+++ debug.py
@@ -1,10 +1,6 @@
-from src.train import train_epoch
+from src.train import train
 from src.parse_args import parse_args
-from src.pipeline import load_dataset, build_model, build_optimizer
 
 
 args = parse_args()
-train_loader, val_loader, test_loader = load_dataset(args)
-superpoint, superglue = build_model(args)
-optimizer, scheduler = build_optimizer(superglue, args)
-train_epoch(superpoint, superglue, train_loader, optimizer, scheduler, args)
+train(args)
